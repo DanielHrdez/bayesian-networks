05bf766d62b5c07be997ee17e20160d99108f12b
Adrian Fleitas <alu0101024363@ull.edu.es>
2022-04-12 23:22:03 +0100

    出来上がり

--- src/bayesian-network.py
+++ src/bayesian-network.py
@@ -9,13 +9,58 @@ Authores:
   - Adrián Fleitas de la Rosa
 """
 
+import pysmile_license
 import pysmile
 
-# net = pysmile.Network()
-# net.readFile('model/seminario.xdsl')
-# https://support.bayesfusion.com/pysmile-B/pysmile/
-# https://download.bayesfusion.com/files.html?category=Academia
+HEARD_NEAR = "HN"
+HEALTH = "Health"
+WEAPON = "Weapon"
+PROXIMITY_HEALTH = "proximity_health"
+PROXIMITY_WEAPON = "proximity_weapon"
+NUMBER_ENEMIES = "Number_enemies"
+OPONENT_WEAPON = "Oponent_weapon"
+ST = "st"
+ST_1 = "st_1"
 
-# .pyd: This is basically a windows dll file. http://docs.python.org/faq/windows.html#is-a-pyd-file-the-same-as-a-dll
+ARGUMENT_LIST = [
+  ST,
+  HEALTH,
+  WEAPON,
+  OPONENT_WEAPON,
+  HEARD_NEAR,
+  NUMBER_ENEMIES,
+  PROXIMITY_WEAPON,
+  PROXIMITY_HEALTH,
+]
 
- DLL load failed while importing pysmile
+net = pysmile.Network()
+net.read_file('../model/seminario.xdsl')
+net.update_beliefs()
+
+def calculate_next_value(values):
+  for i in range(len(values)):
+    net.set_evidence(ARGUMENT_LIST[i], values[i])
+  net.update_beliefs()
+  beliefs = net.get_node_value(ST_1)
+  max_value = 0
+  max_belief = ''
+  for i in range(len(beliefs)):
+    if (beliefs[i] > max_value):
+      max_value = beliefs[i]
+      max_belief = net.get_outcome_id(ST_1, i)
+  
+  return max_belief
+
+if __name__ == '__main__':
+  introduce = 'Introduce el valor para '
+  St = input(f'{introduce} el State: [Attack | grab_weapon | grab_energy | explore | run_away | detect_danger]\n')
+  H = input(f'{introduce} Health: [high | low]\n')
+  W = input(f'{introduce} Weapon: [armed | disarmed]\n')
+  OW = input(f'{introduce} Oponent Weapon: [armed | disarmed]\n')
+  HN = input(f'{introduce} Hear Near: [yes | no]\n')
+  NE = input(f'{introduce} Near Enemies: [yes | no]\n')
+  PW = input(f'{introduce} Proximity Weapon: [yes | no]\n')
+  PH = input(f'{introduce} Proximity Health: [yes | no]\n')
+  St_next = calculate_next_value([St, H, W, OW, HN, NE, PW, PH])
+  print(f'El valor para St_next es: {St_next}')
+  
---
+++ src/test.py
@@ -0,0 +1,5 @@
+import pysmile
+import pysmile_license
+
+net = pysmile.Network()
+net.readFile('model/seminario.xdsl')
