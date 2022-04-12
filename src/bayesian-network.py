a217492c046e9410d2cf8f006055491421588406
Daniel Hernandez de Leon <alu0101331720@ull.edu.es>
2022-04-12 21:18:39 +0100

    added header

---
+++ src/bayesian-network.py
@@ -0,0 +1,10 @@
+"""
+Universidad de La Laguna
+Escuela Superior de Ingeniería y Tecnología
+Grado en Ingeniería Informática
+Asignatura: Inteligencia Artificial Avanzada
+Programa para el uso de redes bayesianas.
+Authores:
+  - Daniel Hernández de Elón
+  - Adrián Fleitas de la Rosa
+"""
