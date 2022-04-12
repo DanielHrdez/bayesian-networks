20986f4e79da7038cf457a84dadf105c1e2e7a39
Daniel Hernandez de Leon <alu0101331720@ull.edu.es>
2022-04-12 22:08:20 +0100

    addedcositas 😋

--- src/bayesian-network.py
+++ src/bayesian-network.py
@@ -8,3 +8,14 @@ Authores:
   - Daniel Hernández de Elón
   - Adrián Fleitas de la Rosa
 """
+
+import pysmile
+
+# net = pysmile.Network()
+# net.readFile('model/seminario.xdsl')
+# https://support.bayesfusion.com/pysmile-B/pysmile/
+# https://download.bayesfusion.com/files.html?category=Academia
+
+# .pyd: This is basically a windows dll file. http://docs.python.org/faq/windows.html#is-a-pyd-file-the-same-as-a-dll
+
+ DLL load failed while importing pysmile
